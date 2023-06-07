6af1c60c08247a63a2b1f60a276b8a1581a95625
MrreenHieurr <minhhieu@MrreenHieurrs-MacBook-Air.local>
2023-06-07 16:37:46 +0700

    create Page folder, add demo loginPage

--- timing/lib/main.dart
+++ timing/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timeing/PAGE/loginPage.dart';
 
 import 'BackGround/backgroundLogin.dart';
 
@@ -19,76 +20,7 @@ class _MyApp extends State<MyApp> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'login',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-          ),
-          body: Stack(
-            children: [
-              BG_login(),
-              SafeArea(
-                  minimum: const EdgeInsets.all(10),
-                  child: Center(
-                      child: Container(
-                    margin: const EdgeInsets.symmetric(vertical: 200),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                alignLabelWithHint: true,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(9),
-                                    gapPadding: 5.0),
-                                labelText: 'username',
-                                labelStyle: const TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                alignLabelWithHint: true,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(9),
-                                    gapPadding: 5.0),
-                                labelText: 'password',
-                                labelStyle: const TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        ),
-                        Container(
-                          alignment: Alignment.centerRight,
-                          margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                          child: SizedBox(
-                            width: 120,
-                            child: ElevatedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'login',
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold),
-                                )),
-                          ),
-                        )
-                      ],
-                    ),
-                  )))
-            ],
-          )),
+      home: LoginPage()
     );
   }
 }
