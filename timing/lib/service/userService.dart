841030b1080ad924cae0fe6c9cfdd3b14e3afda0
MrreenHieurr <minhhieu@MrreenHieurrs-MacBook-Air.local>
2023-06-13 10:31:33 +0700

    update test api

--- timing/lib/service/userService.dart
+++ timing/lib/service/userService.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class UseService {
@@ -6,13 +8,17 @@ class UseService {
   Future<void> loginUser(
       {required String username, required String password}) async {
     http.Client httpClient = http.Client();
-    var response = await httpClient.post(Uri.parse(url), headers: {
-      'Content-Type': 'application/json',
-    }, body: {
-      'username': username,
-      'password': password
-    });
-
+    final response = await httpClient.post(Uri.parse(url),
+        headers: {"Content-type": "application/json"},
+        body: jsonEncode({
+          'username': 'mhieusz.23.dkc@gmail.com',
+          'password': 'minhhieu2003'
+        }));
+    if (response.statusCode == 200) {
+      print('aduu');
+      final body = jsonDecode(response.body) as Map;
+      print('something is wrong?: ' + body.toString());
+    }
     print('hello ');
   }
 }
